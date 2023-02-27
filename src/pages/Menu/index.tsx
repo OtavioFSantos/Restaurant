f79aa350eda3a27cee76ae197528b17f7586467c
OtavioFSantos <otaviofs01@gmail.com>
2023-02-27 10:52:05 -0300

    add search bar and filters

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -1,15 +1,28 @@
 import styles from "./Menu.module.scss";
 import { ReactComponent as Logo } from "assets/logo.svg";
+import SearchBar from "./SearchBar";
+import { useState } from "react";
+import Filters from "./Filters";
 
 export default function Menu() {
+  const [search, setSearch] = useState("");
+  const [filter, setFilter] = useState<number | null>(null);
+
   return (
     <main>
-      <nav className={styles.menu}>
+      <nav className={styles.logo}>
         <Logo />
       </nav>
       <header className={styles.header}>
         <div className={styles.header__text}>Sample text</div>
       </header>
+      <section className={styles.menu}>
+        <h3 className={styles.menu__title}>Menu</h3>
+        <SearchBar search={search} setSearch={setSearch} />
+        <div className={styles.menu__filters}>
+          <Filters filter={filter} setFilter={setFilter} />
+        </div>
+      </section>
     </main>
   );
 }
