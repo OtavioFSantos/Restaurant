0b3ecdd307fdc9186ce1e0481995fe25ad03b876
OtavioFSantos <otaviofs01@gmail.com>
2023-02-27 10:52:25 -0300

    add search bar component

---
+++ src/pages/Menu/SearchBar/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./SearchBar.module.scss";
+import { CgSearch } from "react-icons/cg";
+
+interface Props {
+  search: string;
+  setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export default function SearchBar({ search, setSearch }: Props) {
+  return (
+    <div className={styles.searchbar}>
+      <input value={search} onChange={(ev) => setSearch(ev.target.value)} />
+      <CgSearch size={20} color="#4c4d5e" />
+    </div>
+  );
+}
