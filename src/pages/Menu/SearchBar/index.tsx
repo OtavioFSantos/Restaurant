1b9e2eb22e8332ed402a159edd3e686070c7d6d8
OtavioFSantos <otaviofs01@gmail.com>
2023-02-28 23:56:52 -0300

    added placeholder

--- src/pages/Menu/SearchBar/index.tsx
+++ src/pages/Menu/SearchBar/index.tsx
@@ -10,7 +10,11 @@ interface Props {
 export default function SearchBar({ search, setSearch }: Props) {
   return (
     <div className={styles.searchbar}>
-      <input value={search} onChange={(ev) => setSearch(ev.target.value)} />
+      <input
+        value={search}
+        onChange={(ev) => setSearch(ev.target.value)}
+        placeholder="Search"
+      />
       <CgSearch size={20} color="#4c4d5e" />
     </div>
   );
